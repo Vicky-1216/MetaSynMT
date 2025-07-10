96e6be68df05b3a5f36d1d7fa156eebc57d4d70c
Vicky-1216 <19862153096@163.com>
2025-07-10 18:20:34 +0800

    Update target.py

--- target.py
+++ target.py
@@ -37,40 +37,30 @@ for drug, targets in result_mapping.items():
 
 
 
-#
+
 # import pandas as pd
 #
-# # 读取drug-target.csv文件
 # drug_target_df = pd.read_csv('./static/drug-target.csv')
 #
-# # 将靶标名分割为列表，并创建字典
 # drug_target_dict = {
 #     drug: target.split(',') for drug, target in zip(drug_target_df['drug'], drug_target_df['target'])
 # }
 #
-# # 读取test.csv文件
 # test_df = pd.read_csv('./static/test_drug_drug_samples_name_top.csv')
 #
-# # 获取test.csv中第一列和第二列的药物名，并取并集
 # unique_drug_names = set(test_df.iloc[:, 0]).union(set(test_df.iloc[:, 1]))
 #
-# # 定义一个函数，用于查找药物对应的靶标列表
 # def find_targets(drug_name, drug_target_dict):
 #     return drug_target_dict.get(drug_name, [])
 #
-# # 创建一个空的DataFrame来存储结果
 # result_df = pd.DataFrame(columns=['Drug', 'Targets'])
 #
-# result_rows = []  # 创建一个空列表来存储每一行的数据
 # for drug_name in unique_drug_names:
 #     targets = find_targets(drug_name, drug_target_dict)
 #     result_rows.append({'Drug': drug_name, 'Targets': targets})
 #
-# # 使用concat将列表转换为DataFrame
 # result_df = pd.concat([pd.DataFrame(result_rows)], ignore_index=True)
 #
-# # 重置索引并丢弃旧索引
 # result_df.reset_index(drop=True, inplace=True)
 #
-# # 显示结果
 # print(result_df)
